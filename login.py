e73debf7133230de09ba48cac6583da5eb2d76c0
Suprima-Shrestha <suprima0110@gmail.com>
2025-07-06 15:05:13 +0545

    practices

---
+++ login.py
@@ -0,0 +1,26 @@
+from password import mail,pw
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+driver=webdriver.Edge()
+driver.get("https://authorized-partner.netlify.app/")
+driver.maximize_window()
+wait=WebDriverWait(driver,10)
+try:
+    #login
+    driver.find_element(By.XPATH,"//p[text()='Login']").click()
+    email=wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='email']")))
+    email.send_keys(mail)
+    password=driver.find_element(By.ID,"password")
+    password.send_keys(pw)
+    button=driver.find_element(By.XPATH,"//button[@type='submit']")
+    button.click()
+    assert "login" in driver.current_url,"URL does not contain 'login'"
+    print("Test executed successfully")
+except AssertionError as ae:
+    print(f"Assertion failed:{ae}")
+except Exception as e:
+    print(f"Test failed:{e}")
+
+
